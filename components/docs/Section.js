c1a0ce609601a7b118860775d37c8e23369a5f7a
Manu Anish <manusanish@gmail.com>
2022-04-24 08:22:29 -0400

    Passed linting

--- components/docs/Section.js
+++ components/docs/Section.js
@@ -1,5 +1,5 @@
 export default function Section(props){
   return (
-    <h5 class="mb-8 lg:mb-3 font-semibold text-slate-200 mt-10">{props.title}</h5>
+    <h5 className="mb-8 lg:mb-3 font-semibold text-slate-200 mt-10">{props.title}</h5>
   )
 }
--- components/home/Header.js
+++ components/home/Header.js
@@ -4,7 +4,7 @@ export default function Header(){
   return (
     <div className="relative pt-6 lg:pt-8 flex items-center justify-between text-slate-700 font-semibold text-sm leading-6 text-slate-100">
       <div className='pl-8 font-bold text-lg whitespace-nowrap shadow-lg mr-10 relative'>
-        <span className="flex"><svg className="fill-orange-500" stroke-width="0" viewBox="0 0 384 512" height="24px" xmlns="http://www.w3.org/2000/svg"><path d="M216 23.86c0-23.8-30.65-32.77-44.15-13.04C48 191.85 224 200 224 288c0 35.63-29.11 64.46-64.85 63.99-35.17-.45-63.15-29.77-63.15-64.94v-85.51c0-21.7-26.47-32.23-41.43-16.5C27.8 213.16 0 261.33 0 320c0 105.87 86.13 192 192 192s192-86.13 192-192c0-170.29-168-193-168-296.14z"></path></svg>&#8239;&#8202;Bon5R</span>
+        <span className="flex"><svg className="fill-orange-500" strokeWidth="0" viewBox="0 0 384 512" height="24px" xmlns="http://www.w3.org/2000/svg"><path d="M216 23.86c0-23.8-30.65-32.77-44.15-13.04C48 191.85 224 200 224 288c0 35.63-29.11 64.46-64.85 63.99-35.17-.45-63.15-29.77-63.15-64.94v-85.51c0-21.7-26.47-32.23-41.43-16.5C27.8 213.16 0 261.33 0 320c0 105.87 86.13 192 192 192s192-86.13 192-192c0-170.29-168-193-168-296.14z"></path></svg>&#8239;&#8202;Bon5R</span>
       </div>
       <div className="flex items-center" style={{overflowX: 'scroll'}}>
         <div className="flex items-center">
