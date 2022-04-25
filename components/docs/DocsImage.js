5ffdc67707986764ce134effb5c6d1ef3417c26a
Manu Anish <manusanish@gmail.com>
2022-04-25 13:14:30 -0400

    Fixed bug.

--- components/docs/DocsImage.js
+++ components/docs/DocsImage.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function DocsImage(props) {
   return (
     <div className="relative height-[100px] width-[100px] flex justify-center">
-      <Image src={props.src} alt={props.alt} width={props.width} height={props.height} placeholder="blur"/>
+      <Image src={props.src} alt={props.alt} width={props.width} height={props.height}/>
     </div>
   );
 }
