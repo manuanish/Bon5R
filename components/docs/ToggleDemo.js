b3d346164d342ad87241d6ef02aaa388a8fdb68a
Manu Anish <manusanish@gmail.com>
2022-06-14 16:23:39 -0400

    Updating docs for Bon5R v2.0.0

--- components/docs/ToggleDemo.js
+++ components/docs/ToggleDemo.js
@@ -1,37 +1,49 @@
-import { GeistProvider, CssBaseline, Button, Toggle, Card, Text, Divider } from '@geist-ui/core'
+import {
+  GeistProvider,
+  CssBaseline,
+  Button,
+  Toggle,
+  Card,
+  Text,
+  Divider,
+} from "@geist-ui/core";
 import * as React from "react";
 import { Sun, Moon } from "@geist-ui/icons";
 
 export default function ToggleDemo(props) {
-  const [theme, setTheme] = React.useState('light')
+  const [theme, setTheme] = React.useState("light");
 
   const handleClick = () => {
-    if (theme == 'light') {
-      setTheme('dark')
+    if (theme == "light") {
+      setTheme("dark");
     } else {
-      setTheme('light')
+      setTheme("light");
     }
-  }
+  };
 
   return (
     <div>
       <GeistProvider themeType={theme}>
-      <Card>
-      <div className="flex">
-      <div className="flex grow">
-        <Text type="secondary">{props.title}</Text>
-      </div>
-      <div className="flex gap-2">
-        <Sun />
-        <Toggle scale={1.4} checked={false} onChange={handleClick} />
-        <Moon />
-      </div>
-      </div>
-      <Divider h="1px" my={0} />
-      <div className={`w-full flex justify-center ${props.padding}  ${theme == "light" ? `bg-[#FFFFFF]` : `bg-[#000000]`}`}>
-        {theme == "light" ? props.childrenLight : props.childrenDark}
-      </div>
-      </Card>
+        <Card>
+          <div className="flex">
+            <div className="flex grow">
+              <Text type="secondary">{props.title}</Text>
+            </div>
+            <div className="flex gap-2">
+              <Sun />
+              <Toggle scale={1.4} checked={false} onChange={handleClick} />
+              <Moon />
+            </div>
+          </div>
+          <Divider h="1px" my={0} />
+          <div
+            className={`w-full flex justify-center p-${props.padding}  ${
+              theme == "light" ? `bg-[#FFFFFF]` : `bg-[#000000]`
+            }`}
+          >
+            {theme == "light" ? props.childrenLight : props.childrenDark}
+          </div>
+        </Card>
       </GeistProvider>
     </div>
   );
--- components/docs/bon5r/Header.js
+++ components/docs/bon5r/Header.js
@@ -1,12 +1,59 @@
 import * as React from "react";
+import { Sun, Moon } from "@geist-ui/icons";
+import { Toggle, Divider, GeistProvider } from "@geist-ui/core";
+import { useRouter } from "next/router";
 
 function Header(props) {
+  const [checked, setChecked] = React.useState(true);
+  const [theme, setTheme] = React.useState("light");
+  const router = useRouter();
+
+  React.useEffect(() => {
+    setTheme(localStorage.getItem("theme"));
+    if (
+      localStorage.getItem("theme") == undefined ||
+      localStorage.getItem("theme") == "dark"
+    ) {
+      setTheme("dark");
+      setChecked(true);
+    } else {
+      setTheme("light");
+      setChecked(false);
+    }
+  });
+
+  const handleClick = () => {
+    if (checked == false) {
+      localStorage.setItem("theme", "dark");
+      router.push(router.pathname);
+    } else {
+      localStorage.setItem("theme", "light");
+      router.push(router.pathname);
+    }
+  };
+
   return (
-    <div>
-      <div className="bg-white p-8 w-full z-50 border-b">
-        <div className="font-bold">{props.children}</div>
+    <GeistProvider themeType={theme}>
+      <div className="duration-500">
+        <div
+          className={`top-0 left-0 p-8 w-full z-50 ${
+            theme == "dark"
+              ? "bg-[#000000] border-b-2 border-[#111111]"
+              : "bg-[#FFFFFF] border-b-2 border-[#FAFAFA]"
+          }`}
+        >
+          <div className="flex w-full gap-10">
+            <div className="font-bold grow">{props.children}</div>
+            <div className="flex gap-2">
+              <Sun />
+              <Toggle checked={checked} onClick={handleClick} scale={1.4} />
+              <Moon />
+            </div>
+          </div>
+        </div>
+        <div className="pb-[150px]"></div>
       </div>
-    </div>
+    </GeistProvider>
   );
 }
 
